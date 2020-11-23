50264f8ad34582ee279d3f2c84391dd6a2cd53d9
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-23 16:07:29 +0100

    option

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 var app =  new Vue ({
     el: '#root',
     data: {
-        discs: []
+        discs: [],
+        genre: []
     },
     mounted() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then(result =>{
             this.discs = result.data.response;
             console.log(this.discs);
+
+            //ciclo l'array di discs per ottenre il genre
+            //dopo aver creato un'array per il genre
+            //faccio push se il genre non è ancora presente nell'array
+            this.discs.forEach((item, i) => {
+                console.log(item.genre);
+                if (!this.genre.includes(item.genre)) {
+                    this.genre.push(item.genre)
+                }
+
+            });
+
         });
     }
 });
