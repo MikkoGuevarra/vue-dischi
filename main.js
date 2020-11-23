1de447d743fb358ba18b7d31fe44ab9ac9f844e4
Mikko Paulo Guevarra <emphigie10@gmail.com>
2020-11-23 17:26:03 +0100

    not working

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ var app =  new Vue ({
     el: '#root',
     data: {
         discs: [],
-        genre: []
+        genre: [],
+        selectedValue: false
+    },
+    methods : {
+        onChange(event) {
+            console.log(event.target.value);
+            this.discs.forEach((item, i) => {
+                if (item.genre == event.target.value) {
+                    selectedValue = true;
+                    console.log(selectedValue);
+                }
+
+            });
+        }
     },
     mounted() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
         .then(result =>{
             this.discs = result.data.response;
-            console.log(this.discs);
 
             //ciclo l'array di discs per ottenre il genre
             //dopo aver creato un'array per il genre
             //faccio push se il genre non è ancora presente nell'array
             this.discs.forEach((item, i) => {
-                console.log(item.genre);
                 if (!this.genre.includes(item.genre)) {
                     this.genre.push(item.genre)
                 }
